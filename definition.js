aadd56bb9151d69c47ccda030e17b447c1e94661
Minh Ha <minhha@aitt.vn>
2021-11-09 09:52:40 +0700

    Update Plant:Bit v2

--- definition.js
+++ definition.js
@@ -1,5 +1,5 @@
 // Cảm biến độ ẩm đất
-Blockly.Blocks['plantbit_soil_sensor'] = {
+Blockly.Blocks['plantbit2_soil_sensor'] = {
   init: function() {
     this.jsonInit(
       {
@@ -34,7 +34,7 @@ Blockly.Blocks['plantbit_soil_sensor'] = {
   }
 };
 
-Blockly.Python['plantbit_soil_sensor'] = function(block) {
+Blockly.Python['plantbit2_soil_sensor'] = function(block) {
   Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
   var dropdown_name = block.getFieldValue('NAME');
   // TODO: Assemble Python into code variable.
@@ -44,7 +44,7 @@ Blockly.Python['plantbit_soil_sensor'] = function(block) {
 };
 
 // Cảm biến ánh sáng
-Blockly.Blocks['plantbit_light_sensor'] = {
+Blockly.Blocks['plantbit2_light_sensor'] = {
   init: function() {
     this.jsonInit(
       {
@@ -79,7 +79,7 @@ Blockly.Blocks['plantbit_light_sensor'] = {
   }
 };
 
-Blockly.Python['plantbit_light_sensor'] = function(block) {
+Blockly.Python['plantbit2_light_sensor'] = function(block) {
   Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
   var dropdown_name = block.getFieldValue('NAME');
   // TODO: Assemble Python into code variable.
@@ -88,7 +88,7 @@ Blockly.Python['plantbit_light_sensor'] = function(block) {
   return [code, Blockly.Python.ORDER_NONE];
 };
 
-Blockly.Blocks["plantbit_dht_create"] = {
+Blockly.Blocks["plantbit2_dht_create"] = {
  
   init: function() {
     this.jsonInit({
@@ -128,34 +128,29 @@ Blockly.Blocks["plantbit_dht_create"] = {
       inputsInline: false,
       previousStatement: null,
       nextStatement: null,
-      colour: 120,
+      colour: "#009900",
       tooltip: Blockly.Msg.BLOCK_DHT_CREATE_TOOLTIP,
       helpUrl: Blockly.Msg.BLOCK_DHT_CREATE_HELPURL
     });
   }
 };
 
-Blockly.Blocks["plantbit_dht_measure"] = {
+Blockly.Blocks["plantbit2_dht_measure"] = {
   init: function() {
     this.jsonInit({
       message0: Blockly.Msg.BLOCK_DHT_MEANSURE_MESSAGE0,
       args0: [
-      {
-        type: "field_variable",
-        name: "dht_sensor",
-        variable: Blockly.Msg.BLOCK_DHT_MEANSURE_MESSAGE1
-      }
       ],
       previousStatement: null,
       nextStatement: null,
-      colour: 120,
+      colour: "#009900",
       tooltip: Blockly.Msg.BLOCK_DHT_MEANSURE_TOOLTIP,
       helpUrl: Blockly.Msg.BLOCK_DHT_MEANSURE_HELPURL
     });
   }
 };
 
-Blockly.Blocks["plantbit_dht_read"] = {
+Blockly.Blocks["plantbit2_dht_read"] = {
   init: function() {
     this.jsonInit({
       message0: Blockly.Msg.BLOCK_DHT_READ_MESSAGE0,
@@ -167,15 +162,10 @@ Blockly.Blocks["plantbit_dht_read"] = {
             [Blockly.Msg.BLOCK_DHT_READ_MESSAGE1, "TEMP"],
             [Blockly.Msg.BLOCK_DHT_READ_MESSAGE2, "HUMID"]
           ]
-        },
-        {
-          type: "field_variable",
-          name: "dht_sensor",
-          variable: Blockly.Msg.BLOCK_DHT_READ_MESSAGE3
         }
       ],
       output: null,
-      colour: 120,
+      colour: "#009900",
       tooltip: Blockly.Msg.BLOCK_DHT_READ_TOOLTIP,
       helpUrl: Blockly.Msg.BLOCK_DHT_READ_HELPURL
     });
@@ -183,38 +173,43 @@ Blockly.Blocks["plantbit_dht_read"] = {
 };
 
 
-Blockly.Python["plantbit_dht_create"] = function(block) {
+Blockly.Python["plantbit2_dht_create"] = function(block) {
   var variable_dht = Blockly.Python.variableDB_.getName(block.getFieldValue('dht_sensor'), Blockly.Variables.NAME_TYPE);
   var dropdown_port = block.getFieldValue("port");
   // TODO: Assemble Python into code variable.
   Blockly.Python.definitions_["import_yolobit"] = "from yolobit import *";
   Blockly.Python.definitions_["import_time"] = "import time";
-  Blockly.Python.definitions_["import_dht"] = "import dht";
-  var code =  variable_dht + " = dht.DHT11" + "(Pin(" + dropdown_port + ".pin))\n";
+  Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin, SoftI2C';
+  Blockly.Python.definitions_["import_dht20"] = "from plantbit2_dht20 import DHT20";
+  var code =  variable_dht + " = DHT20(SoftI2C(scl=Pin(22), sda=Pin(21)))\n";
   return code;
 };
 
-Blockly.Python["plantbit_dht_measure"] = function(block) {
-  var variable_dht = Blockly.Python.variableDB_.getName(block.getFieldValue('dht_sensor'), Blockly.Variables.NAME_TYPE);
+Blockly.Python["plantbit2_dht_measure"] = function(block) {
   // TODO: Assemble Python into code variable.
-  var code = variable_dht + ".measure()\n";
+  Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin, SoftI2C';
+  Blockly.Python.definitions_["import_dht20"] = "from plantbit2_dht20 import DHT20";
+  Blockly.Python.definitions_["import_create_dht20"] = "dht20 = DHT20(SoftI2C(scl=Pin(22), sda=Pin(21)))";
+  var code = "dht20.dht20_measure()\n";
   return code;
 };
 
-Blockly.Python["plantbit_dht_read"] = function(block) {
+Blockly.Python["plantbit2_dht_read"] = function(block) {
   var dropdown_data = block.getFieldValue("DATA");
-  var variable_dht = Blockly.Python.variableDB_.getName(block.getFieldValue('dht_sensor'), Blockly.Variables.NAME_TYPE);
   // TODO: Assemble Python into code variable.
+  Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin, SoftI2C';
+  Blockly.Python.definitions_["import_dht20"] = "from plantbit2_dht20 import DHT20";
+  Blockly.Python.definitions_["import_create_dht20"] = "dht20 = DHT20(SoftI2C(scl=Pin(22), sda=Pin(21)))";
   var code = "";
   if (dropdown_data == "TEMP")
-    code = variable_dht + ".temperature()";
+    code = "dht20.dht20_temperature()";
   else 
-    code = variable_dht + ".humidity()";
+    code = "dht20.dht20_humidity()";
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };
 
-Blockly.Blocks['plantbit_ultrasonic_create'] = {
+Blockly.Blocks['plantbit2_ultrasonic_create'] = {
   /**
    * Block for waiting.
    * @this Blockly.Block
@@ -222,13 +217,17 @@ Blockly.Blocks['plantbit_ultrasonic_create'] = {
   init: function() {
     this.jsonInit(
       {
-        "type": "plantbit_ultrasonic_create",
+        "type": "plantbit2_ultrasonic_create",
         "message0": Blockly.Msg.ULTRASONIC_CREATE_MESSAGE0,
         "args0": [
           {
             "type": "field_dropdown",
             "name": "TRG",
             "options": [
+              [
+                "P10",
+                "pin10"
+              ],
               [
                 "P0",
                 "pin0"
@@ -268,10 +267,6 @@ Blockly.Blocks['plantbit_ultrasonic_create'] = {
               [
                 "P9",
                 "pin9"
-              ],
-              [
-                "P10",
-                "pin10"
               ],
               [
                 "P11",
@@ -312,6 +307,10 @@ Blockly.Blocks['plantbit_ultrasonic_create'] = {
             "name": "ECH",
             "options": [
               [
+                "P13",
+                "pin13"
+              ],
+              [
                 "P0",
                 "pin0"
               ],
@@ -362,10 +361,6 @@ Blockly.Blocks['plantbit_ultrasonic_create'] = {
               [
                 "P12",
                 "pin12"
-              ],
-              [
-                "P13",
-                "pin13"
               ],
               [
                 "P14",
@@ -400,17 +395,17 @@ Blockly.Blocks['plantbit_ultrasonic_create'] = {
   }
 };
 
-Blockly.Python['plantbit_ultrasonic_create'] = function(block) {
+Blockly.Python['plantbit2_ultrasonic_create'] = function(block) {
   var dropdown_trg = block.getFieldValue('TRG');
   var dropdown_ech = block.getFieldValue('ECH');
   // TODO: Assemble Python into code variable.
   Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
-  Blockly.Python.definitions_['import_ultrasonic'] = 'from hcsr04 import HCSR04';
+  Blockly.Python.definitions_['import_ultrasonic'] = 'from plantbit2_hcsr04 import HCSR04';
   var code = 'ultrasonic_plant_bit = HCSR04(trigger_pin=' + dropdown_trg + '.pin, echo_pin=' + dropdown_ech + '.pin)\n';
   return code;
 };
 
-Blockly.Blocks['plantbit_ultrasonic_read'] = {
+Blockly.Blocks['plantbit2_ultrasonic_read'] = {
   init: function() {
     this.jsonInit(
       {
@@ -441,7 +436,7 @@ Blockly.Blocks['plantbit_ultrasonic_read'] = {
   }
 };
 
-Blockly.Python['plantbit_ultrasonic_read'] = function(block) {
+Blockly.Python['plantbit2_ultrasonic_read'] = function(block) {
   var dropdown_type = block.getFieldValue('TYPE');
   // TODO: Assemble Python into code variable.
   var code = '';
@@ -454,11 +449,11 @@ Blockly.Python['plantbit_ultrasonic_read'] = function(block) {
   return [code, Blockly.Python.ORDER_NONE];
 };
 
-Blockly.Blocks['plantbit_ultrasonic_checkdistance'] = {
+Blockly.Blocks['plantbit2_ultrasonic_checkdistance'] = {
   init: function() {
     this.jsonInit(
       {
-        "type": "plantbit_ultrasonic_checkdistance",
+        "type": "plantbit2_ultrasonic_checkdistance",
         "message0": Blockly.Msg.ULTRASONIC_CHECK_MESSAGE0,
         "args0": [
           {
@@ -493,21 +488,21 @@ Blockly.Blocks['plantbit_ultrasonic_checkdistance'] = {
   }
 };
 
-Blockly.Python['plantbit_ultrasonic_checkdistance'] = function(block) {
+Blockly.Python['plantbit2_ultrasonic_checkdistance'] = function(block) {
   var value_distance = Blockly.Python.valueToCode(block, 'DISTANCE', Blockly.Python.ORDER_ATOMIC);
   var dropdown_type = block.getFieldValue('TYPE');
   // TODO: Assemble Python into code variable.
   var code = '';
   if (dropdown_type == 'CM')
-    code = 'ultrasonic_plant_bit.distance_cm() < ' + value_distance;
+    code = 'ultrasonic_plant_bit.distance_cm() > ' + value_distance;
   else
-    code = 'ultrasonic_plant_bit.distance_mm()' + value_distance;
+    code = 'ultrasonic_plant_bit.distance_mm() > ' + value_distance;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };
 
 
-Blockly.Blocks['plantbit_ultrasonic_checkwater'] = {
+Blockly.Blocks['plantbit2_ultrasonic_checkwater'] = {
   init: function() {
     this.jsonInit(
       {
@@ -532,7 +527,7 @@ Blockly.Blocks['plantbit_ultrasonic_checkwater'] = {
   }
 };
 
-Blockly.Python['plantbit_ultrasonic_checkwater'] = function(block) {
+Blockly.Python['plantbit2_ultrasonic_checkwater'] = function(block) {
   var number_min = Blockly.Python.valueToCode(block, 'A', Blockly.Python.ORDER_ATOMIC);
   var number_max = Blockly.Python.valueToCode(block, 'B', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
@@ -542,18 +537,13 @@ Blockly.Python['plantbit_ultrasonic_checkwater'] = function(block) {
 };
 
 
-Blockly.Blocks['plantbit_oled_create'] = {
+Blockly.Blocks['plantbit2_oled_create'] = {
   init: function() {
     this.jsonInit(
       {
         "type": "plantbit_oled_create",
         "message0": Blockly.Msg.OLED_CREATE_MESSAGE0,
         "args0": [
-          {
-            "type": "field_variable",
-            "name": "oled_lcd",
-            "variable": Blockly.Msg.OLED_CREATE_MESSAGE1,
-          }
         ],
         "previousStatement": null,
         "nextStatement": null,
@@ -565,29 +555,23 @@ Blockly.Blocks['plantbit_oled_create'] = {
   }
 };
 
-Blockly.Python['plantbit_oled_create'] = function(block) {
-  var variable_oled_lcd = Blockly.Python.variableDB_.getName(block.getFieldValue('oled_lcd'), Blockly.Variables.NAME_TYPE);
+Blockly.Python['plantbit2_oled_create'] = function(block) {
   // TODO: Assemble Python into code variable.
   Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
-  Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin, I2C';
-  Blockly.Python.definitions_['import_oled'] = 'from ssd1306 import SSD1306_I2C';
+  Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin, SoftI2C';
+  Blockly.Python.definitions_['import_oled'] = 'from plantbit2_ssd1306 import SSD1306_I2C';
   // oled = SSD1306_I2C(oled_width, oled_height, i2c)
-  var code = variable_oled_lcd + ' = SSD1306_I2C( 128, 64 , I2C(-1, scl=Pin(22), sda=Pin(21)))\n';
+  var code = 'lcd_oled = SSD1306_I2C( 128, 64 , SoftI2C(scl=Pin(22), sda=Pin(21)))\n';
   return code;
 };
 
-Blockly.Blocks['plantbit_oled_text'] = {
+Blockly.Blocks['plantbit2_oled_text'] = {
   init: function() {
     this.jsonInit(
       {
-        "type": "plantbit_oled_text",
+        "type": "plantbit2_oled_text",
         "message0": Blockly.Msg.OLED_TEXT_MESSAGE0,
         "args0": [
-          {
-            "type": "field_variable",
-            "name": "oled_lcd",
-            "variable": Blockly.Msg.OLED_TEXT_MESSAGE1,
-          },
           {
             "type": "input_value",
             "name": "TEXT"
@@ -614,29 +598,23 @@ Blockly.Blocks['plantbit_oled_text'] = {
   }
 };
 
-Blockly.Python['plantbit_oled_text'] = function(block) {
-  var variable_oled_lcd = Blockly.Python.variableDB_.getName(block.getFieldValue('oled_lcd'), Blockly.Variables.NAME_TYPE);
+Blockly.Python['plantbit2_oled_text'] = function(block) {
   var value_text = Blockly.Python.valueToCode(block, 'TEXT', Blockly.Python.ORDER_ATOMIC);
   var value_x = Blockly.Python.valueToCode(block, 'X', Blockly.Python.ORDER_ATOMIC);
   var value_y = Blockly.Python.valueToCode(block, 'Y', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
   //oled.text('Hello, World 1!', 0, 0, col); oled.show()
-  var code = variable_oled_lcd + '.text(str(' + value_text + '), ' + value_x + ', ' + value_y + ', 1); ' + variable_oled_lcd + '.show()\n';
+  var code = 'lcd_oled.text(str(' + value_text + '), ' + value_x + ', ' + value_y + ', 1); lcd_oled.show()\n';
   return code;
 };
 
-Blockly.Blocks['plantbit_oled_fill'] = {
+Blockly.Blocks['plantbit2_oled_fill'] = {
   init: function() {
     this.jsonInit(
       {
-        "type": "plantbit_oled_fill",
+        "type": "plantbit2_oled_fill",
         "message0": Blockly.Msg.OLED_FILL_MESSAGE0,
         "args0": [
-          {
-            "type": "field_variable",
-            "name": "oled_lcd",
-            "variable": Blockly.Msg.OLED_FILL_MESSAGE1,
-          }
         ],
         "previousStatement": null,
         "nextStatement": null,
@@ -648,15 +626,15 @@ Blockly.Blocks['plantbit_oled_fill'] = {
   }
 };
 
-Blockly.Python['plantbit_oled_fill'] = function(block) {
+Blockly.Python['plantbit2_oled_fill'] = function(block) {
   var variable_oled_lcd = Blockly.Python.variableDB_.getName(block.getFieldValue('oled_lcd'), Blockly.Variables.NAME_TYPE);
   // TODO: Assemble Python into code variable.
   //oled.fill(1); oled.show()
-  var code = variable_oled_lcd + '.fill(0); ' + variable_oled_lcd + '.show()\n';
+  var code = 'lcd_oled.fill(0); lcd_oled.show()\n';
   return code;
 };
 
-Blockly.Blocks['plantbit_dual_usb_1'] = {
+Blockly.Blocks['plantbit2_dual_usb_pump'] = {
   /**
    * Block for waiting.
    * @this Blockly.Block
@@ -664,13 +642,17 @@ Blockly.Blocks['plantbit_dual_usb_1'] = {
   init: function() {
     this.jsonInit(
       {
-        "type": "plantbit_dual_usb_1",
-        "message0": "bật máy bơm ở chân %1 ở mức (1-100) %2 %%",
+        "type": "plantbit2_dual_usb_pump",
+        "message0": "bật máy bơm ở chân %1 với tốc độ (0-100) %2 %%",
         "args0": [
           {
             "type": "field_dropdown",
             "name": "NAME",
             "options": [
+              [
+                "P14",
+                "pin14"
+              ],
               [
                 "P0",
                 "pin0"
@@ -728,10 +710,6 @@ Blockly.Blocks['plantbit_dual_usb_1'] = {
                 "pin13"
               ],
               [
-                "P14",
-                "pin14"
-              ],
-              [
                 "P15",
                 "pin15"
               ],
@@ -765,7 +743,7 @@ Blockly.Blocks['plantbit_dual_usb_1'] = {
   }
 };
 
-Blockly.Python['plantbit_dual_usb_1'] = function(block) {
+Blockly.Python['plantbit2_dual_usb_pump'] = function(block) {
   Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
   var dropdown_name = block.getFieldValue('NAME');
   var value_percent = Blockly.Python.valueToCode(block, 'percent', Blockly.Python.ORDER_ATOMIC);
@@ -774,7 +752,7 @@ Blockly.Python['plantbit_dual_usb_1'] = function(block) {
   return code;
 };
 
-Blockly.Blocks['plantbit_dual_usb_2'] = {
+Blockly.Blocks['plantbit2_dual_usb_led'] = {
   /**
    * Block for waiting.
    * @this Blockly.Block
@@ -782,13 +760,17 @@ Blockly.Blocks['plantbit_dual_usb_2'] = {
   init: function() {
     this.jsonInit(
       {
-        "type": "plantbit_dual_usb_2",
-        "message0": "bật đèn LED màu ở chân %1 ở mức (1-100) %2 %%",
+        "type": "plantbit2_dual_usb_led",
+        "message0": "bật đèn LED màu ở chân %1 với mức sáng (0-100) %2 %%",
         "args0": [
           {
             "type": "field_dropdown",
             "name": "NAME",
             "options": [
+              [
+                "P15",
+                "pin15"
+              ],
               [
                 "P0",
                 "pin0"
@@ -850,10 +832,6 @@ Blockly.Blocks['plantbit_dual_usb_2'] = {
                 "pin14"
               ],
               [
-                "P15",
-                "pin15"
-              ],
-              [
                 "P16",
                 "pin16"
               ],
@@ -883,7 +861,7 @@ Blockly.Blocks['plantbit_dual_usb_2'] = {
   }
 };
 
-Blockly.Python['plantbit_dual_usb_2'] = function(block) {
+Blockly.Python['plantbit2_dual_usb_led'] = function(block) {
   Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
   var dropdown_name = block.getFieldValue('NAME');
   var value_percent = Blockly.Python.valueToCode(block, 'percent', Blockly.Python.ORDER_ATOMIC);
